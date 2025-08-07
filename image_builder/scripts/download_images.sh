fd618df7d527d9c1bdf79c89a515fbbbe9f56593
sarikap9 <sarika@platform9.com>
2025-08-07 16:12:52 +0530

    platform9->sarika-p9

--- image_builder/scripts/download_images.sh
+++ image_builder/scripts/download_images.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 TAG=$1
 
 REGISTRY="quay.io"
-REPO="platform9"
+REPO="sarika-p9"
 kube_state_metrics="registry.k8s.io/kube-state-metrics/kube-state-metrics:v2.13.0"
 prometheus_adapter="registry.k8s.io/prometheus-adapter/prometheus-adapter:v0.12.0"
 prometheus="quay.io/prometheus/prometheus:v2.54.1"
@@ -22,13 +22,13 @@ prometheus_config_reloader="quay.io/prometheus-operator/prometheus-config-reload
 prometheus_operator="quay.io/prometheus-operator/prometheus-operator:v0.76.0"
 configmap_reload="ghcr.io/jimmidyson/configmap-reload:v0.13.1"
 grafana="docker.io/grafana/grafana:11.2.0"
-v2v_helper="quay.io/platform9/vjailbreak-v2v-helper:$TAG"
-controller="quay.io/platform9/vjailbreak-controller:$TAG"
-ui="quay.io/platform9/vjailbreak-ui:$TAG"
-vpwned="quay.io/platform9/vjailbreak-vpwned:$TAG"
+v2v_helper="quay.io/sarika-p9/vjailbreak-v2v-helper:$TAG"
+controller="quay.io/sarika-p9/vjailbreak-controller:$TAG"
+ui="quay.io/sarika-p9/vjailbreak-ui:$TAG"
+vpwned="quay.io/sarika-p9/vjailbreak-vpwned:$TAG"
 virtiowin="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso"
 # TODO(suhas): Create a seperate repository for alpine image in quay
-alpine="quay.io/platform9/vjailbreak:alpine"
+alpine="quay.io/sarika-p9/vjailbreak:alpine"
 
 # Download and export images
 images=(
